5814e64646c37486f28f2a68c370787e18fe4fa5
adamsoderstrom <adamsoderstrom@me.com>
2024-02-23 23:40:40 +0100

    chore(chck): convert to shell functions

--- scripts/chck.sh
+++ scripts/chck.sh
@@ -1,5 +1,12 @@
 # chck
 # ----
 # The Centra Checkout API URL of the current directory's environment files.
-alias chcku="echo $(cat $(envf) | rg '^CHECKOUT_API=(.*)' -or '$1' || cat $(envf) | rg '^CENTRA_CHECKOUT_API=(.*)' -or '$1')"
-alias chckt="echo $(cat $(envf) | rg '^CHECKOUT_API_KEY=(.*)' -or '$1' || cat $(envf) | rg '^CENTRA_CHECKOUT_API_KEY=(.*)' -or '$1')"
+function chcku() {
+  file=$(envf)
+  echo $(cat $file | rg '^CHECKOUT_API=(.*)' -or '$1' || cat $file | rg '^CENTRA_CHECKOUT_API=(.*)' -or '$1') 
+}
+
+function chckt() {
+  file=$(envf)
+  echo $(cat $file | rg '^CHECKOUT_API_KEY=(.*)' -or '$1' || cat $file | rg '^CENTRA_CHECKOUT_API_KEY=(.*)' -or '$1')
+}
