d86a20eb28cf5bb5fe416b5f0b2ff5ca32e9c241
adamsoderstrom <adamsoderstrom@me.com>
2024-02-25 22:16:50 +0100

    feat(noamux): add project directory constraint + navigation

--- scripts/noamux.sh
+++ scripts/noamux.sh
@@ -1,19 +1,27 @@
-# @TODO: Check if $1 is a directory in NoA/projects
+#!/bin/bash
+
 function noa() {
+  if [[ -d "$HOME/Documents/NoA/Projects/$1" ]];
+  then
+    create_attach_switch_to_tmux_session $1
+  else
+    echo "$1 isn't found among NoA projects"
+    return 1
+  fi
+}
+
+function create_attach_switch_to_tmux_session() {
   tmux has-session -t $1 2>/dev/null
   SESSION_EXISTS=$?
-  echo $SESSION_EXISTS
   
     # If TMUX is attached.
   if [[ ! -z "${TMUX}" ]]; then
-    echo 'Tmux is attached'
-
     if [ $SESSION_EXISTS = 0 ]; then
         # And session does exist, then switch to session.
         tmux switch -t $1
       else
         # If session doesn't exist, then create it.
-        tmux new-session -s $1 -d
+        tmux new-session -s $1 -d -c "$HOME/Documents/NoA/Projects/$1"
         # and then switch to it.
         tmux switch -t $1
     fi
@@ -26,7 +34,7 @@ function noa() {
     else
       # If session doesn't exist.
       # Then create new session and attach it.
-      tmux new-session -s $1
+      tmux new-session -s $1 -c "$HOME/Documents/NoA/Projects/$1"
     fi
   fi
 }
