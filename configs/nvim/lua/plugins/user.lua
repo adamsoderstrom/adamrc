5c96b0ac442a67e28b33a61d6d21c502b140b966
adamsoderstrom <adamsoderstrom@me.com>
2024-10-02 06:49:53 +0200

    [nvim] remove `andweeb/presence` and `ray-x/lsp_signature`

--- configs/nvim/lua/plugins/user.lua
+++ configs/nvim/lua/plugins/user.lua
@@ -4,15 +4,6 @@
 ---@type LazySpec
 return {
 
-  -- == Examples of Adding Plugins ==
-
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
-
   -- == Examples of Overriding Plugins ==
 
   -- customize alpha options
