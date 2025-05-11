d72a3dbaada8d39f11787f690811d0ae0adce45f
adamsoderstrom <adamsoderstrom@me.com>
2025-05-11 17:51:39 +0200

    [scripts]: rename `noamux` to `ign`

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,5 +1,8 @@
 export SCRIPTS_DIR=`dirname "$0"`
 
+export PATH=$PATH:~/.cargo/bin
+export PATH=$PATH:$(go env GOPATH)/bin
+
 source $SCRIPTS_DIR/bat.sh
 source $SCRIPTS_DIR/cgpt.sh
 source $SCRIPTS_DIR/chck.sh
@@ -8,11 +11,11 @@ source $SCRIPTS_DIR/envf.sh
 source $SCRIPTS_DIR/ffmpeg.sh
 source $SCRIPTS_DIR/gh.sh
 source $SCRIPTS_DIR/git_snippets.sh
+source $SCRIPTS_DIR/ign.sh
 source $SCRIPTS_DIR/lenv.sh
 source $SCRIPTS_DIR/misc.sh
 source $SCRIPTS_DIR/next.sh
 source $SCRIPTS_DIR/noa.sh
-source $SCRIPTS_DIR/noamux.sh
 source $SCRIPTS_DIR/nxb.sh
 source $SCRIPTS_DIR/pplx.sh
 source $SCRIPTS_DIR/sb-story.sh
@@ -21,9 +24,6 @@ source $SCRIPTS_DIR/smackdown.sh
 source $SCRIPTS_DIR/t.sh
 source $SCRIPTS_DIR/vopen.sh
 source $SCRIPTS_DIR/y.sh
-
-export PATH=$PATH:~/.cargo/bin
-export PATH=$PATH:$(go env GOPATH)/bin
 
 export fpath=($SCRIPTS_DIR/../completions $fpath)
 
