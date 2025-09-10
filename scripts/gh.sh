9b0182737193faf506709c563676dccc2d3d6f22
adamsoderstrom <adamsoderstrom@me.com>
2025-09-10 11:33:50 +0200

    [gh] add `ghconflict`
    To list all open PRs with merge conflicts

--- scripts/gh.sh
+++ scripts/gh.sh
@@ -6,3 +6,6 @@ alias ghprw="gh pr view --web"
 
 # ghprc - View a GitHub pull request
 alias ghprc="gh pr create"
+
+alias ghconflict="gh pr list --state open --json number,title,mergeable \
+  | jq '.[] | select(.mergeable==\"CONFLICTING\") | {number, title}'"
