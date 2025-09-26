53240e6002e2eeb7eab200612da9e131cdce2fe7
adamsoderstrom <adamsoderstrom@me.com>
2025-09-26 22:34:03 +0200

    [gh]: update `ghconflict`

--- scripts/gh.sh
+++ scripts/gh.sh
@@ -7,5 +7,4 @@ alias ghprw="gh pr view --web"
 # ghprc - View a GitHub pull request
 alias ghprc="gh pr create"
 
-alias ghconflict="gh pr list --state open --json number,title,mergeable \
-  | jq '.[] | select(.mergeable==\"CONFLICTING\") | {number, title}'"
+alias ghconflict="gh pr list --state open --json number,title,mergeable | jq -r '.[] | select(.mergeable==\"CONFLICTING\") | [.number, .title] | @tsv'"
