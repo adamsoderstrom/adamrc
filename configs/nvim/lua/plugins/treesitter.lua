78aca21a40033128dc6b8e311d5d37b66af567f5
adamsoderstrom <adamsoderstrom@me.com>
2025-09-26 22:35:57 +0200

    [nvim]: add `dac` - Delete around function / method call

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,18 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Treesitter
-
----@type LazySpec
+-- in your user config: ~/.config/nvim/lua/plugins/treesitter.lua
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
+  opts = function(_, opts)
+    -- make sure textobjects is there
+    opts.textobjects = opts.textobjects or {}
+    opts.textobjects.select = vim.tbl_deep_extend("force", opts.textobjects.select or {}, {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        -- calls
+        ["ac"] = "@call.outer",
+        ["ic"] = "@call.inner",
+      },
+    })
+    return opts
+  end,
 }
