ccc636a3ffbd55a4ea2149df4931265a560880b3
adamsoderstrom <adamsoderstrom@me.com>
2025-11-05 12:16:30 +0100

    [vars]: add `centra_checkout_swagger_url`

--- vars.sh
+++ vars.sh
@@ -1,2 +1,4 @@
 # A JQ command to get sizes of each key in a JSON object.
 export JQ_SIZE="[[to_entries | .[] | [.key, (.value | tostring | length)]] | sort_by(.[1])]"
+
+export centra_checkout_swagger_url="https://demo-docs.centra.com/api/checkout/swagger"
