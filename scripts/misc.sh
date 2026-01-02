ae476d5f9ae8b83da17e3302a8dc7c5035cc8dbb
adamsoderstrom <adamsoderstrom@me.com>
2026-01-02 14:44:22 +0100

    open current shell command for editing in `nvim`
    Source: https://gist.github.com/elliottminns/09a598082d77f795c88e93f7f73dba61

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -7,3 +7,9 @@ alias refresh="source ~/.zshrc"
 alias nowplaying="nowplaying-cli"
 alias adambrew="brew bundle dump --force --file=~/.adamrc/Brewfile"
 alias cal="gcal --starting-day=1"
+
+# Open the current command in your $EDITOR (e.g., neovim)
+# Press Ctrl+X followed by Ctrl+E to trigger
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey '^E' edit-command-line
