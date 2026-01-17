359a0c7ab3757b94844c19cc1e7812472a1ff57b
adamsoderstrom <adamsoderstrom@me.com>
2026-01-17 09:39:26 +0100

    [nvim]: install `javascript`, `typescript` and `tsx` for treesitter

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,13 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     -- make sure textobjects is there
+    if not opts.ensure_installed then opts.ensure_installed = {} end
+    vim.list_extend(opts.ensure_installed, {
+      "javascript",
+      "tsx",
+      "typescript",
+      -- add more arguments for adding more treesitter parsers
+    })
     opts.textobjects = opts.textobjects or {}
     opts.textobjects.select = vim.tbl_deep_extend("force", opts.textobjects.select or {}, {
       enable = true,
